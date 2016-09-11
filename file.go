19dbb940e4836449f4495a9bfba3ca149845e6d8
xujh945@qq.com <xujianhui0915>
2016-09-11 19:32:14 +0800

    String As FilePath

---
+++ file.go
@@ -0,0 +1,64 @@
+package Figo
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type FilePath string
+
+func (p FilePath) FullPath() (string, error) {
+	f, err := p.Open()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Abs(f.Name())
+}
+
+func (p FilePath) UnixPath() string {
+	return strings.Replace(p.String(), "\\", "/", -1)
+}
+
+func (p FilePath) WindowsPath() string {
+	return strings.Replace(p.String(), "/", "\\", -1)
+}
+
+func (p FilePath) FileName() string {
+	toks := strings.Split(p.UnixPath(), "/")
+	return toks[len(toks)-1]
+}
+
+func (p FilePath) FolderName() string {
+	return strings.Replace(p.String(), p.FileName(), "", -1)
+}
+
+func (p FilePath) Exist() bool {
+	var exist = true
+	if _, err := os.Stat(p.String()); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+func (p FilePath) String() string {
+	return string(p)
+}
+
+func (p FilePath) Open() (*os.File, error) {
+	var file *os.File
+	var err error
+	if p.Exist() {
+		file, err = os.OpenFile(p.String(), os.O_RDWR, 0666)
+	} else {
+		if err := os.MkdirAll(p.FolderName(), 0777); err != nil {
+			return nil, err
+		}
+		file, err = os.Create(p.String()) //创建文件
+	}
+	return file, err
+}
+
+func FileOpen(s string) (*os.File, error) {
+	var filepath FilePath = FilePath(s)
+	return filepath.Open()
+}
---
+++ file_test.go
@@ -0,0 +1,21 @@
+package Figo
+
+import (
+	"github.com/figoxu/utee"
+	"log"
+	"testing"
+)
+
+func TestFileOpen(t *testing.T) {
+	_, err := FileOpen("./test.txt")
+	utee.Chk(err)
+	fp := FilePath("./test.txt")
+	fullPath, err := fp.FullPath()
+	utee.Chk(err)
+	log.Println(fullPath)
+	fp = FilePath(fullPath)
+	log.Println(fp.WindowsPath())
+	log.Println(fp.UnixPath())
+	fp = FilePath(fp.UnixPath())
+	log.Println(fp.WindowsPath())
+}
