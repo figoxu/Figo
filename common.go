554c4776738aa59be2bd7bef419bf01a3c410097
Figo.Xu <xujh945@qq.com>
2016-11-02 11:41:06 +0800

    Add Clone Method With Unit Test

--- common.go
+++ common.go
@@ -3,10 +3,16 @@ package Figo
 import (
 	"log"
 	"runtime/debug"
+	//	"github.com/hailocab/gocassa/reflect"
 )
 
 func Catch() {
 	err := recover()
 	log.Println(string(debug.Stack()))
 	log.Println(err, " (recover)")
+}
+
+func Clone(src interface{}) interface{} {
+	dst := (src)
+	return dst
 }
--- common_test.go
+++ common_test.go
@@ -1,10 +1,14 @@
 package Figo
 
 import (
+	"fmt"
 	"github.com/gogap/errors"
 	"github.com/quexer/utee"
 	"log"
+	"math/rand"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestCatch(t *testing.T) {
@@ -17,4 +21,72 @@ func catchSampleMethod() {
 	defer Catch()
 	log.Println("Hello")
 	utee.Chk(errors.New("Some Error"))
+}
+
+type SubCloneObj struct {
+	sync.RWMutex
+	data  map[string]string
+	key   string
+	score int
+}
+
+type CloneObj struct {
+	sync.RWMutex
+	Leader   SubCloneObj
+	Member   []SubCloneObj
+	TeamName string
+	Year     int
+}
+
+func (p *CloneObj) display() {
+	fmt.Println("@Year:", p.Year, " @TeamName:", p.TeamName, "@Leader:", p.Leader)
+	for _, obj := range p.Member {
+		fmt.Println(obj)
+	}
+}
+
+func (p *CloneObj) AppendChild(objs []SubCloneObj) {
+	log.Println("===>")
+	log.Println(p.Member)
+	log.Println("<===")
+	p.Member = append(p.Member, objs...)
+}
+
+func RandomCloneObj() SubCloneObj {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	rv := func(v string) string {
+		return fmt.Sprint(v, "-", r.Int())
+	}
+	obj := SubCloneObj{
+		data:  map[string]string{rv("foo"): rv("bar"), rv("foo1"): rv("bar1"), rv("foo2"): rv("bar2")},
+		key:   rv("Figo"),
+		score: r.Intn(100),
+	}
+	return obj
+}
+
+func RandomCloneObjs(length int) []SubCloneObj {
+	objs := []SubCloneObj{}
+	for i := 0; i < length; i++ {
+		objs = append(objs, RandomCloneObj())
+	}
+	return objs
+}
+
+func TestClone(t *testing.T) {
+	cloneObj := CloneObj{
+		Leader:   RandomCloneObj(),
+		TeamName: "Bee",
+		Year:     2016,
+		Member:   RandomCloneObjs(1),
+	}
+	obj2 := Clone(cloneObj).(CloneObj)
+	obj2.AppendChild(RandomCloneObjs(2))
+	obj2.Year = 2017
+
+	log.Println("===============")
+	cloneObj.display()
+	log.Println("===============")
+	obj2.display()
+
 }
