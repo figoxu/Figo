27b45589be9b809af36262c08f24ddc84731e60f
Figo.Xu <xujh945@qq.com>
2016-11-14 17:53:52 +0800

    Add Sentry TestCase

--- cache-sentry_test.go
+++ cache-sentry_test.go
@@ -1,1 +1,28 @@
-package Figo_test
+package Figo
+
+import (
+	"github.com/quexer/utee"
+	"log"
+	"testing"
+)
+
+func TestSentry(t *testing.T) {
+	tc := utee.NewTimerCache(60, nil)
+	put := func(key, val interface{}) {
+		tc.Put(key, val)
+	}
+	get := func(key interface{}) interface{} {
+		return tc.Get(key)
+	}
+	cacheObj := NewCacheObj(put, get)
+
+	notify := func(key interface{}) error {
+		log.Println("need to notify @key:", key, " to clear the cache")
+		return nil
+	}
+	sentry := NewSentry(notify, cacheObj)
+	sentry.Put("hello", "world")
+	sentry.Put("hello", "Cool")
+	v := sentry.Get("hello")
+	log.Println(v)
+}
