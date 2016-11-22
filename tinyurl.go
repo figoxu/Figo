412a336ed4510d3e3e4c2a3fe6f2f9c32b0937f9
maoyu.zhang <maoyu.zhang@tendcloud.com>
2016-11-22 16:00:38 +0800

    用redis的inrc生成tinyurl的key

--- tinyurl.go
+++ tinyurl.go
@@ -1,10 +1,10 @@
 package Figo
 
 import (
-	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"github.com/go-martini/martini"
-	"github.com/pborman/uuid"
 	"net/http"
+	"strconv"
 )
 
 const (
@@ -19,16 +19,18 @@ func NewTinyUrl(cache Cache) tinyUrl {
 	return tinyUrl{cache: cache}
 }
 
-func (t tinyUrl) Convert(url string) string {
-	uid := uuid.NewUUID().String()
-	k := fmt.Sprintf("%s-%s", PREFIX, uid)
-	t.cache.Put(k, url)
-	return k
+func (t tinyUrl) Convert(url string, rp *redis.Pool) string {
+	//uid := uuid.NewUUID().String()
+	//k := fmt.Sprintf("%s-%s", PREFIX, uid)
+	s := &SequenceObj{}
+	k := s.Next(rp)
+	t.cache.Put(strconv.FormatInt(k, 16), url)
+	return strconv.FormatInt(k, 16)
 }
 
 func (t tinyUrl) GetRedirectUrlHandler(key string) martini.Handler {
-	originUrl := t.getUrl(key)
 	return func(w http.ResponseWriter, r *http.Request) {
+		originUrl := t.getUrl(key)
 		http.Redirect(w, r, originUrl, 301)
 	}
 }
