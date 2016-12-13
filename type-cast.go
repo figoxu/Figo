9f5233fa329515f84bd20e8f408b6442923b792f
Figo.Xu <xujh945@qq.com>
2016-12-13 17:00:04 +0800

    Add Type Cast For Interface

--- type-cast.go
+++ type-cast.go
@@ -13,3 +13,32 @@ func TpString(reply interface{}) (string, error) {
 	}
 	return "", fmt.Errorf("redigo: unexpected type for String, got type %T", reply)
 }
+
+var ICast = InterfaceCast{}
+
+type InterfaceCast struct {
+}
+
+func (p *InterfaceCast) FromString(v string) interface{} {
+	return interface{}(v)
+}
+
+func (p *InterfaceCast) FromInt(v int) interface{} {
+	return interface{}(v)
+}
+
+func (p *InterfaceCast) FromStrings(data []string) []interface{} {
+	s := make([]interface{}, len(data))
+	for i, v := range data {
+		s[i] = v
+	}
+	return s
+}
+
+func (p *InterfaceCast) FromInts(data []int) []interface{} {
+	s := make([]interface{}, len(data))
+	for i, v := range data {
+		s[i] = v
+	}
+	return s
+}
