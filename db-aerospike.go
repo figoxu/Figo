34c6ad90b882aab77cc9233fd82f4363af83bdcd
Figo.Xu <xujh945@qq.com>
2016-12-15 11:27:05 +0800

    Test Aerospike Util

--- db-aerospike.go
+++ db-aerospike.go
@@ -7,9 +7,13 @@ import (
 	"log"
 )
 
-var AS = &AerospikeUtil{}
+var AsUtee = &AerospikeUtil{}
 
 type AerospikeUtil struct{}
+type AsSetInfo struct {
+	NameSpace string
+	SetName   string
+}
 
 func (p *AerospikeUtil) AsConnect(s string) *as.Client {
 	h, port, err := utee.ParseUrl(s)
@@ -27,12 +31,36 @@ func (p *AerospikeUtil) InitLua(ac *as.Client, serverPath, udfCode string) {
 	utee.Chk(err)
 }
 
-func (p *AerospikeUtil) Index(ac *as.Client, namespace, setName, binName string, tp as.IndexType) {
-	if idxTask, err := ac.CreateIndex(nil, namespace, setName, setName+binName, binName, tp); err == nil {
+func (p *AerospikeUtil) Index(ac *as.Client, setInfo AsSetInfo, binName string, tp as.IndexType) {
+	if idxTask, err := ac.CreateIndex(nil, setInfo.NameSpace, setInfo.SetName, setInfo.SetName+binName, binName, tp); err == nil {
 		<-idxTask.OnComplete()
 	} else {
 		if asErr, ok := err.(types.AerospikeError); !ok || asErr.ResultCode() != types.INDEX_FOUND {
 			panic(err)
 		}
+	}
+}
+
+func (p *AerospikeUtil) Put(ac *as.Client, setInfo AsSetInfo, key string, val interface{}) error {
+	asKey, err := as.NewKey(setInfo.NameSpace, setInfo.SetName, key)
+	switch v := val.(type) {
+	case as.BinMap:
+		err = ac.Put(nil, asKey, v)
+	default:
+		err = ac.PutObject(nil, asKey, val)
+	}
+	return err
+}
+
+func (p *AerospikeUtil) Get(ac *as.Client, setInfo AsSetInfo, key string, val interface{}) error {
+	asKey, err := as.NewKey(setInfo.NameSpace, setInfo.SetName, key)
+	ac.GetObject(nil, asKey, val)
+	return err
+}
+
+func (p *AerospikeUtil) NewSetInfo(namespace, setName string) AsSetInfo {
+	return AsSetInfo{
+		NameSpace: namespace,
+		SetName:   setName,
 	}
 }
---
+++ db-aerospike_test.go
@@ -0,0 +1,20 @@
+package Figo
+
+import (
+	"log"
+	"testing"
+)
+
+func TestAs(t *testing.T) {
+	ac := AsUtee.AsConnect("106.75.27.144:3000")
+	setInfo := AsUtee.NewSetInfo("push", "test")
+	type Val struct {
+		Offset int64
+	}
+	err := AsUtee.Put(ac, setInfo, "hello", &Val{1027})
+	log.Println("Put Err @err:", err)
+	var val Val
+	err = AsUtee.Get(ac, setInfo, "hello", &val)
+	log.Println("Get Err @err:", err)
+	log.Println(val)
+}
