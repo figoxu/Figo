fbcac41f125deebb4625f20eaf8a45840afc00d8
Figo.Xu <xujh945@qq.com>
2016-12-26 18:06:54 +0800

    Remove Depency Of iconv ,Let Api Cross Platform

--- string.go
+++ string.go
@@ -2,24 +2,9 @@ package Figo
 
 import (
 	"fmt"
-	"github.com/quexer/utee"
-	"gopkg.in/iconv.v1"
-	"log"
 	"regexp"
 	"strings"
-	"unicode/utf8"
 )
-
-func DealStringMessy(result string) string {
-	if !utf8.ValidString(result) {
-		cd, err := iconv.Open("utf-8", "gbk")
-		utee.Chk(err)
-		defer cd.Close()
-		result = cd.ConvString(result)
-		log.Println("Parse From GBK To  UTF-8")
-	}
-	return result
-}
 
 func SplitUTF8BOM(str string) string {
 	b := []byte(str)
