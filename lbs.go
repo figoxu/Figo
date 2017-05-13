044baa24e1378c029879cf3fdaede2f8d95dd743
Figo.Xu <xujh945@qq.com>
2017-05-13 08:40:55 +0800

    Add Lbs Distance Func

---
+++ lbs.go
@@ -0,0 +1,20 @@
+package Figo
+
+import "github.com/kellydunn/golang-geo"
+
+type GeoPoint struct {
+	Lat float64
+	Lng float64
+}
+
+func NewGeoPoint(lat ,lng float64)GeoPoint{
+	return GeoPoint{
+		Lat:lat,
+		Lng:lng,
+	}
+}
+
+func GeoDistance(from ,to GeoPoint)float64{
+	return geo.NewPoint(from.Lat, from.Lng).GreatCircleDistance(geo.NewPoint(to.Lat,to.Lng))
+}
+
