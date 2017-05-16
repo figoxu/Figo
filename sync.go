87ef65ea0dfc670b7811ddc8699cf2b2e9730f16
Figo.Xu <xujh945@qq.com>
2017-05-16 10:12:54 +0800

    Fix Lock Api

--- sync.go
+++ sync.go
@@ -1,29 +1,43 @@
 package Figo
 
-import "github.com/garyburd/redigo/redis"
+import (
+	"github.com/garyburd/redigo/redis"
+)
 
 type RedisMutex struct {
 	rp       *redis.Pool
 	resource string
 }
 
-func NewRedisMutex(rp *redis.Pool,resource string)*RedisMutex{
+func NewRedisMutex(rp *redis.Pool, resource string) *RedisMutex {
 	return &RedisMutex{
-		rp:rp,
-		resource:resource,
+		rp:       rp,
+		resource: resource,
 	}
 }
 
 func (p *RedisMutex) Lock(ttlSec int) (bool, error) {
-	c := p.rp.Get()
-	defer c.Close()
-	if _, err := redis.String(c.Do("SET", p.resource, "1", "EX", ttlSec, "NX")); err != nil {
-		if err == redis.ErrNil {
-			return false, nil
+	lockAction := func() (bool, error) {
+		c := p.rp.Get()
+		defer c.Close()
+		if _, err := redis.String(c.Do("SET", p.resource, "1", "EX", ttlSec, "NX")); err != nil {
+			if err == redis.ErrNil {
+				return false, nil
+			}
+			return false, err
 		}
-		return false, err
+		return true, nil
 	}
-	return true, nil
+	lock := func() (bool, error) {
+		for {
+			if b, err := lockAction(); b == true {
+				return true, nil
+			} else if err != nil {
+				return false, nil
+			}
+		}
+	}
+	return lock()
 }
 
 func (p *RedisMutex) Unlock() {
