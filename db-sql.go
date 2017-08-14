8d79889297543280224a6213e1ec6b661689b0cd
figoxu <xujh945@qq.com>
2017-08-14 12:37:03 +0800

    Add SqlBuffer For Sql And Param Append

---
+++ db-sql.go
@@ -0,0 +1,28 @@
+package Figo
+
+import "fmt"
+
+type SqlBuffer struct {
+	query string
+	params []interface{}
+}
+
+func NewSqlBuffer()*SqlBuffer{
+	return &SqlBuffer{
+		query:"",
+		params:make([]interface{},0),
+	}
+}
+
+func (p *SqlBuffer) Append(appendQuery string, appendParams ... interface{}) {
+	p.query = fmt.Sprint(p.query," ",appendQuery," ")
+	p.params = append(p.params,appendParams...)
+}
+
+func (p *SqlBuffer) SQL()string{
+	return p.query
+}
+
+func (p *SqlBuffer) Params()[]interface{}{
+	return p.params
+}
