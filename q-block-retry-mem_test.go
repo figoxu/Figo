b0b0db6ec979b47dc3357d5d291bb307f068eb03
figoxu <xujh945@qq.com>
2017-10-23 16:33:15 +0800

    Add Test Case

---
+++ q-block-retry-mem_test.go
@@ -0,0 +1,29 @@
+package Figo
+
+import (
+	"testing"
+	"github.com/Pallinder/go-randomdata"
+	"log"
+	"fmt"
+)
+
+func TestNewBlockExecuteQ(t *testing.T) {
+
+	bq := NewBlockExecuteQ(1000, 3, 3, func(v interface{})bool {
+		b := randomdata.Boolean()
+		if b {
+			log.Println("execute @v:", v, " SUCCESS")
+		} else {
+			log.Println("execute @v:", v, " FAILURE")
+		}
+		return b
+	})
+
+	mockInput := func() {
+		for i := 0; i < 30; i++ {
+			bq.Enq(fmt.Sprint("testData ", i))
+		}
+	}
+	mockInput()
+	<-make(chan bool)
+}
