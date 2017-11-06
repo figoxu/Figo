261fe7d6c840a808e9321668fddf493fc0e15a96
figoxu <xujh945@qq.com>
2017-11-06 20:47:50 +0800

    增加GORM的默认日志Writer

---
+++ db-gorm.go
@@ -0,0 +1,23 @@
+package Figo
+
+import (
+	"log"
+	"github.com/jinzhu/gorm"
+)
+
+type GormLog struct {
+
+}
+
+
+// @index: 0    @value: sql
+// @index: 1    @value: /Users/xujianhui/GoglandProjects/sdz-stock-service/dao/db-CampSite.go:117
+// @index: 2    @value: 106.335985ms
+// @index: 3    @value:  select * from camp_site order by id
+// @index: 4    @value: []   => result type
+// @index: 5    @value: 102
+func (p *GormLog) Print(values ...interface{}) {
+	log.Println(gorm.LogFormatter(values...)...)
+}
+
+
