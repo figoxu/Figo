a9785c22ef40ecb850538adaba19d6609685e6ed
figoxu <xujh945@qq.com>
2018-01-05 09:09:22 +0800

    Add ReadWrite Lock Of BothWayMap

--- map-bothway.go
+++ map-bothway.go
@@ -2,18 +2,21 @@ package Figo
 
 import (
 	"github.com/quexer/utee"
+	"sync"
 )
 
 type BothWayMap struct {
-	kv  map[interface{}]interface{}
-	vk  map[interface{}]interface{}
-	ttl *utee.TimerCache
+	kv     map[interface{}]interface{}
+	vk     map[interface{}]interface{}
+	ttl    *utee.TimerCache
+	rwlock sync.RWMutex
 }
 
 func NewBothWayMap(ttlTime int) *BothWayMap {
 	bothWayMap := &BothWayMap{
-		kv: make(map[interface{}]interface{}),
-		vk: make(map[interface{}]interface{}),
+		kv:     make(map[interface{}]interface{}),
+		vk:     make(map[interface{}]interface{}),
+		rwlock: sync.RWMutex{},
 	}
 	if ttlTime > 0 {
 		bothWayMap.ttl = utee.NewTimerCache(ttlTime, bothWayMap.ttlCb)
@@ -26,6 +29,8 @@ func (p *BothWayMap) ttlCb(key, value interface{}) {
 }
 
 func (p *BothWayMap) Put(key, value interface{}) *BothWayMap {
+	p.rwlock.Lock()
+	defer p.rwlock.Unlock()
 	p.kv[key] = value
 	p.vk[value] = key
 	if p.ttl != nil {
@@ -35,20 +40,28 @@ func (p *BothWayMap) Put(key, value interface{}) *BothWayMap {
 }
 
 func (p *BothWayMap) GetByKey(key interface{}) (value interface{}, exists bool) {
+	p.rwlock.RLock()
+	defer p.rwlock.RUnlock()
 	value, exists = p.kv[key]
 	return
 }
 
 func (p *BothWayMap) GetByValue(value interface{}) (key interface{}, exists bool) {
+	p.rwlock.RLock()
+	defer p.rwlock.RUnlock()
 	key, exists = p.vk[value]
 	return
 }
 
 func (p *BothWayMap) Len() int {
+	p.rwlock.RLock()
+	defer p.rwlock.RUnlock()
 	return len(p.kv)
 }
 
 func (p *BothWayMap) DeleteKey(key interface{}) *BothWayMap {
+	p.rwlock.Lock()
+	defer p.rwlock.Unlock()
 	value, exists := p.kv[key]
 	if exists {
 		delete(p.kv, key)
@@ -61,6 +74,8 @@ func (p *BothWayMap) DeleteKey(key interface{}) *BothWayMap {
 }
 
 func (p *BothWayMap) DeleteValue(value interface{}) *BothWayMap {
+	p.rwlock.Lock()
+	defer p.rwlock.Unlock()
 	key, exists := p.vk[value]
 	if exists {
 		delete(p.kv, key)
