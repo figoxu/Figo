0e6bbda9ed4bffb3f86d9be6079794940ab4a211
figoxu <xujh945@qq.com>
2018-03-08 09:29:04 +0800

    Fix File Create Error

--- file-utee.go
+++ file-utee.go
@@ -24,16 +24,20 @@ func (p *FileUtee) Exist(filename string) bool {
 func (p *FileUtee) MakeFile(dir, fileName string) (*os.File, error) {
 	var f *os.File
 	var err error
+	if !p.Exist(dir) {
+		err = os.MkdirAll(dir, 0777)
+		if err != nil {
+			log.Println("loc 01")
+			return nil, err
+		}
+	}
 	fileFullName := fmt.Sprint(dir, fileName)
 	if p.Exist(fileFullName) { //如果文件存在
 		f, err = os.OpenFile(fileFullName, os.O_RDWR, 0666) //打开文件
 		log.Println("@fileFullPath:", fileFullName, " is exist")
 	} else {
-		err := os.MkdirAll(dir, 0777)
-		if err != nil {
-			return nil, err
-		}
 		f, err = os.Create(fileFullName) //创建文件
+		log.Println("loc 02 @path:",fileFullName)
 	}
 	return f, err
 }
@@ -73,11 +77,9 @@ func (p *FileUtee) ReadLinesSlice(path string) ([]string, error) {
 func (p *FileUtee) WriteLinesSlice(lines []string, path string) error {
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_APPEND, 0660)
 	if err != nil {
-		if !p.Exist(path) {
-			file, err = p.MakeFile("./", path)
-			if err != nil {
-				return err
-			}
+		file, err = NewFilePath(path).Open()
+		if err != nil {
+			return err
 		}
 	}
 	defer file.Close()
--- file-utee_test.go
+++ file-utee_test.go
@@ -8,9 +8,10 @@ import (
 
 func TestFileUtee_WriteLinesSlice(t *testing.T) {
 	futee:=FileUtee{}
-	futee.WriteLinesSlice([]string{"hello","world","how","r","u"},"./test.txt")
-	futee.WriteLinesSlice([]string{"figo","xu","is","awesome"},"./test.txt")
-	txts,err:=futee.ReadLinesSlice("./test.txt")
+	path := "/server/nfs/session/test.txt"
+	futee.WriteLinesSlice([]string{"hello","world","how","r","u"}, path)
+	futee.WriteLinesSlice([]string{"figo","xu","is","awesome"}, path)
+	txts,err:=futee.ReadLinesSlice(path)
 	utee.Chk(err)
 	for _,txt:=range txts{
 		log.Println(txt)
